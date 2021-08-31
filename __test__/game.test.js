d71bdec2f05ecda83cd84242dee2a3d5438d394b
Charlie Weber <charlestweber@gmail.com>
2021-08-31 14:24:50 -0700

    passes test#2 for game.test

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -3,18 +3,25 @@ import { Game } from "../src/game";
 
 describe('Game', () => {
 
+  let reGame;
+
+  beforeEach(() => {
+    reGame = new Game();
+  });
+
   test('should correctly build game object', () => {
-    let game = new Game();
-    expect(game.score).toEqual({ "player1": 0, "player2": 0 })
-    expect(game.turn).toEqual(true)
-    expect(game.activeScore).toEqual(0)
-    expect(game.initialRoll).toEqual(true)
-    expect(game.activePlayer).toEqual("player2")
-    expect(game.dice).toEqual("Roll the dice!")
+  
+    expect(reGame.score).toEqual({ "player1": 0, "player2": 0 })
+    expect(reGame.turn).toEqual(true)
+    expect(reGame.activeScore).toEqual(0)
+    expect(reGame.initialRoll).toEqual(true)
+    expect(reGame.activePlayer).toEqual("player2")
+    expect(reGame.dice).toEqual("Roll the dice!")
   });
 
   test('should display number between 1-6', () =>{
-    let newGame = new Game
-    expect(newGame.roll()).toEqual("7")
-  })
+    reGame.roll();
+    expect(reGame.dice).toBeGreaterThan(0);
+    expect(reGame.dice).toBeLessThan(7);
+  });
 });
