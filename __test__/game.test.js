cc01cb8e3c3eb9fd47e1af787dde25e8f4f3ca62
Charlie Weber <charlestweber@gmail.com>
2021-08-31 13:25:56 -0700

    confirm good fail for game.test #2

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -5,6 +5,16 @@ describe('Game', () => {
 
   test('should correctly build game object', () => {
     let game = new Game();
-    expect(game.score).toEqual({ "player1": 45, "player2": 12 })
+    expect(game.score).toEqual({ "player1": 0, "player2": 0 })
+    expect(game.turn).toEqual(true)
+    expect(game.activeScore).toEqual(0)
+    expect(game.initialRoll).toEqual(true)
+    expect(game.activePlayer).toEqual("player2")
+    expect(game.dice).toEqual("Roll the dice!")
+  });
+
+  test('should display number between 1-6', () =>{
+    let newGame = new Game
+    expect(newGame.roll()).toEqual("7")
   })
 });
